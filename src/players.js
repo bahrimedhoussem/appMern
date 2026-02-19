495430f2fa4d0770992a9bb89165ac9c6eead3f7
bahrimedhoussem <houssembahri011@gmail.com>
2026-02-20 00:15:00 +0100

    players assigmnt

---
+++ src/players.js
@@ -0,0 +1,36 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "PSG",
+    nationality: "Argentin",
+    jerseyNumber: 30,
+    age: 36,
+    imageUrl: "https://link-to-messi-image.com/messi.jpg"
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al Nassr",
+    nationality: "Portugais",
+    jerseyNumber: 7,
+    age: 38,
+    imageUrl: "https://link-to-ronaldo-image.com/ronaldo.jpg"
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "PSG",
+    nationality: "Français",
+    jerseyNumber: 7,
+    age: 24,
+    imageUrl: "https://link-to-mbappe-image.com/mbappe.jpg"
+  },
+  {
+    name: "Erling Haaland",
+    team: "Manchester City",
+    nationality: "Norvégien",
+    jerseyNumber: 9,
+    age: 23,
+    imageUrl: "https://link-to-haaland-image.com/haaland.jpg"
+  }
+];
+
+export default players;
